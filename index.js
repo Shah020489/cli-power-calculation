d1c5ed9e1fc5570dd29b4295f69e05b82a086f09
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-26 10:55:07 +0500

    All_files_Are_merged_As_basic.

---
+++ index.js
@@ -0,0 +1,20 @@
+#! usr/env/node
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    { name: "base",
+        message: "What is the base number?",
+        type: "number"
+    },
+    { name: "power",
+        message: "What is the power number?",
+        type: "number" }
+]);
+if (answer.power === 0) {
+    console.log("The result is 1");
+}
+else if (answer.power === 1) {
+    console.log("The result is " + answer.base);
+}
+else {
+    console.log("The result is " + answer.base ** answer.power);
+}
