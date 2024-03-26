9f0bce60017ae953a7ea55bc731c61a25cc9f3ee
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-26 11:09:57 +0500

    push changes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-#! usr/env/node
-import inquirer from 'inquirer';
-const answer = await inquirer.prompt([
+#! /usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const answer = await inquirer_1.default.prompt([
     { name: "base",
         message: "What is the base number?",
         type: "number"
